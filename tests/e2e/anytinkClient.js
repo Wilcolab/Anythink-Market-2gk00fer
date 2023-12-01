3861c7779d395f08078613d6ca9819e40df43042
WilcoApp <hello@wilco.gg>
2023-12-01 14:50:27 +0000

    Initial commit

--- tests/e2e/anytinkClient.js
+++ tests/e2e/anytinkClient.js
@@ -11,7 +11,7 @@ class AnythinkClient {
   constructor() {
     this.client = axios.create({
       baseURL: "http://localhost:3000",
-      timeout: 1000,
+      timeout: 2000,
     });
   }
 
@@ -123,12 +123,12 @@ class AnythinkClient {
     return result.data?.item;
   }
 
-  async commentOnItem(slug, commenBody, callingUser) {
+  async commentOnItem(slug, commentBody, callingUser) {
     const result = await this.#apiCall({
       method: Method.POST,
       url: `/api/items/${slug}/comments`,
       callingUser,
-      data: { comment: { body: commenBody } },
+      data: { comment: { body: commentBody } },
     });
     return result.data?.comment;
   }
